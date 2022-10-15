ca71b2657bf03b1056e37f0c6a38c0888b507be4
Joe Pringle <joe-pringle@outlook.com>
2022-10-15 18:11:49 +0100

    initial commit 2

---
+++ WindowsInstalls.ps1
@@ -0,0 +1,34 @@
+winget install -e Valve.Steam
+Write-Host "Finished steam"
+
+winget install -e Microsoft.VisualStudioCode
+Write-Host "Finished VisualStudioCode"
+
+winget install -e ShareX.ShareX
+Write-Host "Finished Sharex"
+
+winget install -e Microsoft.PowerToys
+Write-Host "Finished Powertoys"
+
+winget install -e Mozilla.Thunderbird
+Write-Host "Finished Thunderbird"
+
+winget install -e Mozilla.Firefox
+Write-Host "Finished Firefox"
+
+winget install -e Obsidian.Obsidian
+Write-Host "Finished Obsidian"
+
+winget install -e Dropbox.Dropbox
+Write-Host "Finished Dropbox"
+
+winget install -e Git.Git
+
+winget install -e GitHub.GitHubDesktop
+Write-Host "Finished Github"
+winget install -e Discord.Discord
+Write-Host "Finished Discord"
+
+winget install -e Microsoft.Teams
+Write-Host "Finished Teams"
+Write-Host "Finished install"
